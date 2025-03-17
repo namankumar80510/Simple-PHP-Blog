d84aad7d3cfb13def4c9a1a44394daccd1ac8b0d
Naman <namankumar80510@gmail.com>
2025-03-17 15:13:22 +0530

    engine change

--- api/db/migrations/20250316072131_create_initial_tables.php
+++ api/db/migrations/20250316072131_create_initial_tables.php
@@ -20,7 +20,7 @@ final class CreateInitialTables extends AbstractMigration
     public function change(): void
     {
         // Create admin table
-        $admin = $this->table('admin', ['engine' => 'MyISAM', 'collation' => 'utf8_general_ci']);
+        $admin = $this->table('admin', ['engine' => 'InnoDB', 'collation' => 'utf8_general_ci']);
         $admin->addColumn('username', 'string', ['limit' => 255, 'null' => false])
             ->addColumn('password', 'string', ['limit' => 255, 'null' => false])
             ->addColumn('email', 'string', ['limit' => 255, 'null' => false])
@@ -34,14 +34,14 @@ final class CreateInitialTables extends AbstractMigration
             ->create();
 
         // Create page table
-        $page = $this->table('page', ['engine' => 'MyISAM', 'collation' => 'utf8_general_ci']);
+        $page = $this->table('page', ['engine' => 'InnoDB', 'collation' => 'utf8_general_ci']);
         $page->addColumn('page_name', 'string', ['limit' => 255, 'null' => true])
             ->addColumn('content', 'string', ['limit' => 255, 'null' => false])
             ->addIndex(['page_name'], ['unique' => true])
             ->create();
 
         // Create posts table
-        $posts = $this->table('posts', ['engine' => 'MyISAM', 'collation' => 'utf8_general_ci']);
+        $posts = $this->table('posts', ['engine' => 'InnoDB', 'collation' => 'utf8_general_ci']);
         $posts->addColumn('title', 'string', ['limit' => 255, 'null' => false])
             ->addColumn('description', 'text', ['null' => false])
             ->addColumn('slug', 'string', ['limit' => 255, 'null' => true])
